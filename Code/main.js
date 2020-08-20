5ecb428ad7ca677a1e3bb56eafbe4594e5601170
afleck11 <56262215+afleck11@users.noreply.github.com>
2020-08-20 15:58:40 +0200

    Add files via upload

--- Code/main.js
+++ Code/main.js
@@ -1,9 +1,9 @@
-// set the dimensions and margins of the graph
+// Dimensions du graphique et marges
 var margin = {top: 10, right: 30, bottom: 30, left: 90},
     width = 700 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
+// Ajout du svg
 var svg = d3.select("#mon_script")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -12,27 +12,27 @@ var svg = d3.select("#mon_script")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-//Read the data
+//Lecture des données
 d3.csv("Data/Classeur11.csv", function(data) {
 
-    // List of groups (here I have one group per column)
+    
     var allGroup = d3.map(data, function(d){return(d.region)}).keys()
 
-    // add the options to the button
+    // Ajout de la liste déroulante
     d3.select("#selectButton")
       .selectAll('myOptions')
      	.data(allGroup)
       .enter()
     	.append('option')
-      .text(function (d) { return d; }) // text showed in the menu
-      .attr("value", function (d) { return d; }) // corresponding value returned by the button
+      .text(function (d) { return d; }) 
+      .attr("value", function (d) { return d; }) 
 
-    // A color scale: one color for each group
+    // Ajout de l'échelle de couleurs
     var myColor = d3.scaleOrdinal()
       .domain(allGroup)
       .range(d3.schemeSet2);
 
-    // Add X axis --> it is a date format
+    // Axe X
     var x = d3.scaleLinear()
       .domain(d3.extent(data, function(d) { return d.year; }))
       .range([ 0, width ]);
@@ -40,7 +40,7 @@ d3.csv("Data/Classeur11.csv", function(data) {
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x).ticks(17));
 
-    // Add Y axis
+    // Axe Y
 
     var y = d3.scaleLinear()
       .domain([0, d3.max(data, function(d) { return +d.n; })])
@@ -52,7 +52,7 @@ d3.csv("Data/Classeur11.csv", function(data) {
 
 
 
-    // Initialize line with first group of the list
+    
     var line = svg
       .append('g')
       .append("path")
@@ -65,13 +65,13 @@ d3.csv("Data/Classeur11.csv", function(data) {
         .style("stroke-width", 4)
         .style("fill", "none")
 
-    // A function that update the chart
+
     function update(selectedGroup) {
 
-      // Create new data with the selection?
+ 
       var dataFilter = data.filter(function(d){return d.region==selectedGroup})
 
-      // Give these new data to update line
+  
       line
           .datum(dataFilter)
           .transition()
@@ -83,11 +83,11 @@ d3.csv("Data/Classeur11.csv", function(data) {
           .attr("stroke", function(d){ return myColor(selectedGroup) })
     }
 
-    // When the button is changed, run the updateChart function
+   
     d3.select("#selectButton").on("change", function(d) {
-        // recover the option that has been chosen
+   
         var selectedOption = d3.select(this).property("value")
-        // run the updateChart function with this selected option
+   
         update(selectedOption)
     })
 
